590d061cf24b6e6fc70db4ca85ef400774261f3d
Vito Tai <vitotai@mail.com>
2024-09-27 13:22:06 +0800

    save for 0927

--- src/driver/LGFX_custom/LGFX_ESP32_2432S032C.h
+++ src/driver/LGFX_custom/LGFX_ESP32_2432S032C.h
@@ -5,6 +5,81 @@
 #include <LovyanGFX.hpp>
 #include "lv_drv_conf.h"
 
+namespace lgfx
+{
+ inline namespace v1
+ {
+//----------------------------------------------------------------------------
+
+  struct Panel_ILI9341m : public Panel_LCD
+  {
+    Panel_ILI9341m(void)
+    {
+      _cfg.memory_width  = _cfg.panel_width  = 240;
+      _cfg.memory_height = _cfg.panel_height = 320;
+    }
+
+  protected:
+
+    static constexpr uint8_t CMD_FRMCTR1 = 0xB1;
+    static constexpr uint8_t CMD_FRMCTR2 = 0xB2;
+    static constexpr uint8_t CMD_FRMCTR3 = 0xB3;
+    static constexpr uint8_t CMD_INVCTR  = 0xB4;
+    static constexpr uint8_t CMD_DFUNCTR = 0xB6;
+    static constexpr uint8_t CMD_PWCTR1  = 0xC0;
+    static constexpr uint8_t CMD_PWCTR2  = 0xC1;
+    static constexpr uint8_t CMD_PWCTR3  = 0xC2;
+    static constexpr uint8_t CMD_PWCTR4  = 0xC3;
+    static constexpr uint8_t CMD_PWCTR5  = 0xC4;
+    static constexpr uint8_t CMD_VMCTR1  = 0xC5;
+    static constexpr uint8_t CMD_VMCTR2  = 0xC7;
+    static constexpr uint8_t CMD_GMCTRP1 = 0xE0; // Positive Gamma Correction (E0h)
+    static constexpr uint8_t CMD_GMCTRN1 = 0xE1; // Negative Gamma Correction (E1h)
+    static constexpr uint8_t CMD_RDINDEX = 0xD9; // ili9341
+    static constexpr uint8_t CMD_IDXRD   = 0xDD; // ILI9341 only, indexed control register read
+
+    const uint8_t* getInitCommands(uint8_t listno) const override
+    {
+      static constexpr uint8_t list0[] =
+      {
+          0xEF       , 3, 0x03,0x80,0x02,
+          0xCF       , 3, 0x00,0xC1,0x30,
+          0xED       , 4, 0x64,0x03,0x12,0x81,
+          0xE8       , 3, 0x85,0x00,0x78,
+          0xCB       , 5, 0x39,0x2C,0x00,0x34,0x02,
+          0xF7       , 1, 0x20,
+          0xEA       , 2, 0x00,0x00,
+          CMD_PWCTR1,  1, 0x23,
+          CMD_PWCTR2,  1, 0x10,
+          CMD_VMCTR1,  2, 0x3e,0x28,
+          CMD_VMCTR2,  1, 0x86,
+          CMD_FRMCTR1, 2, 0x00,0x13,
+          0xF2       , 1, 0x00,
+
+          CMD_GAMMASET,1, 0x01,  // Gamma set, curve 1
+//          CMD_GMCTRP1,15, 0x0F,0x31,0x2B,0x0C,0x0E,0x08,0x4E,0xF1,0x37,0x07,0x10,0x03,0x0E,0x09,0x00,
+          CMD_GMCTRP1, 15, 0x1F, 0x1A, 0x18, 0x0A, 0x0F, 0x06, 0x45, 0X87, 0x32, 0x0A, 0x07, 0x02, 0x07, 0x05, 0x00,
+//          CMD_GMCTRN1, 15, 0x00,0x0E,0x14,0x03,0x11,0x07,0x31,0xC1,0x48,0x08,0x0F,0x0C,0x31,0x36,0x0F,
+          CMD_GMCTRN1, 15, 0x00, 0x25, 0x27, 0x05, 0x10, 0x09, 0x3A, 0x78, 0x4D, 0x05, 0x18, 0x0D, 0x38, 0x3A, 0x1F,
+          CMD_DFUNCTR, 3, 0x08,0xC2,0x27,
+          CMD_SLPOUT , 0+CMD_INIT_DELAY, 120,    // Exit sleep mode
+          CMD_IDMOFF , 0,
+          CMD_DISPON , 0+CMD_INIT_DELAY, 100,
+          0xFF,0xFF, // end
+      };
+      switch (listno)
+      {
+      case 0: return list0;
+      default: return nullptr;
+      }
+    }
+  };
+
+//----------------------------------------------------------------------------
+ }
+}
+
+
 class LGFX : public lgfx::LGFX_Device
 {
 
@@ -13,7 +88,7 @@ class LGFX : public lgfx::LGFX_Device
 //lgfx::Panel_HX8357B     _panel_instance;
 //lgfx::Panel_HX8357D     _panel_instance;
 //lgfx::Panel_ILI9163     _panel_instance;
-  lgfx::Panel_ILI9341     _panel_instance;
+  lgfx::Panel_ILI9341m     _panel_instance;
 //lgfx::Panel_ILI9342     _panel_instance;
 //lgfx::Panel_ILI9481     _panel_instance;
 //lgfx::Panel_ILI9486     _panel_instance;
