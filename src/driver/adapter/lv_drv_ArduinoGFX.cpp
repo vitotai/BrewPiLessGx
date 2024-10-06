589e3dd42446306a4d8c02327aa58def73893179
Vito Tai <vitotai@mail.com>
2024-10-06 12:28:53 +0800

    add minium restriction of brigtness for ArduinoGFX driver

--- src/driver/adapter/lv_drv_ArduinoGFX.cpp
+++ src/driver/adapter/lv_drv_ArduinoGFX.cpp
@@ -75,7 +75,8 @@ void display_drv_init()
 }
 void display_drv_set_brightness(uint8_t brightness){
 #ifdef GFX_BL
-   ledcWrite(TFT_PWM_CHANNEL_BL, brightness); /* Screen brightness can be modified by adjusting this parameter. (0-255) */
+   if(brightness< MinimumBrightness) brightness= MinimumBrightness;
+      ledcWrite(TFT_PWM_CHANNEL_BL, brightness); /* Screen brightness can be modified by adjusting this parameter. (0-255) */
 #endif
 }
 
